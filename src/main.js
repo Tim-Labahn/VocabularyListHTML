fdf8925e8a2dccc7c6c41fa823d7f89f3f6af5c0
tim-labahn <tim.labahn@freaknet.de>
2023-08-09 11:32:07 +0200

    working on making the list visuable

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
-function displayMessage() {
+const vocabularyList = [];
+
+function addToList() {
   /** @ts-expect-error @type HTMLInputElement*/
-  let themsg = document.getElementById('newVocName');
-  let input = document.getElementById('showinputhere');
-  if (input) {
-    if (themsg) {
-      input.innerHTML = themsg.value;
-    } else {
-      input.innerHTML = 'No message set';
+  const nameInput = document.getElementById('newVocName');
+  /** @ts-expect-error @type HTMLInputElement*/
+  const definitionInput = document.getElementById('newVocDef');
+
+  vocabularyList.push({ name: nameInput.value, definition: definitionInput.value });
+  // showList();
+}
+
+function showList() {
+  const list = document.querySelector('.VocableList');
+  if (list) {
+    const tr = document.createElement('tr');
+    if (tr) {
+      list.appendChild(tr);
+      for (let i = 0; i < vocabularyList.length; i++) {
+        const td = document.createElement('td');
+        td.innerHTML = vocabularyList[1].name + vocabularyList[1].definition;
+        tr.appendChild(td);
+      }
     }
   }
 }
--- src/vocabularyList.js
+++ src/vocabularyList.js
@@ -1,56 +1,53 @@
-const prompt = require("prompt-sync")({ sigint: true });
+const prompt = require('prompt-sync')({ sigint: true });
 
-const vocabularyList = [
-];
+const vocabularyList = [];
 //-------------------------------------------------------------------------
 function showList(arr) {
-    console.log("Inventory stores:", arr.length, "items");
-    for (let i = 0; i < arr.length; i++) {
-        console.log(i + 1 + ".", arr[i].word, "-", arr[i].definition);
-    }
+  console.log('Inventory stores:', arr.length, 'items');
+  for (let i = 0; i < arr.length; i++) {
+    console.log(i + 1 + '.', arr[i].word, '-', arr[i].definition);
+  }
 }
 
 function addNewVocable(word, definition) {
-    vocabularyList.push({ word, definition });
+  vocabularyList.push({ word, definition });
 }
 
 //----------------------------------------------------------------------
 while (true) {
-    console.log("-------------------------------------------------------------------")
-    console.log("Your options: A- add, D- delete, S- Show list, C- Change definition");
-    const event = prompt("What is your choice?").toUpperCase();
+  console.log('-------------------------------------------------------------------');
+  console.log('Your options: A- add, D- delete, S- Show list, C- Change definition');
+  const event = prompt('What is your choice?').toUpperCase();
 
-    if (event === "A") {
-        let name = prompt("What do you wanna add?");
-        let definition = prompt("What is the definition?");
-        addNewVocable(name, definition);
-    } else if (event === "S") {
-        showList(vocabularyList);
-    } else if (event === "D") {
-        showList(vocabularyList);
-        let deleteItem = prompt("Enter the name of what you want to delete?");
-        // let index = findWordIndex(deleteItem);
+  if (event === 'A') {
+    let name = prompt('What do you wanna add?');
+    let definition = prompt('What is the definition?');
+    addNewVocable(name, definition);
+  } else if (event === 'S') {
+    showList(vocabularyList);
+  } else if (event === 'D') {
+    showList(vocabularyList);
+    let deleteItem = prompt('Enter the name of what you want to delete?');
+    // let index = findWordIndex(deleteItem);
 
-        let index = vocabularyList.findIndex((a, i) => a.word === deleteItem || i + 1 === +deleteItem);
-        if (index !== -1) {
-            vocabularyList.splice(index, 1);
-        } else {
-            console.log("Word is not found in the vocabulary list.");
-        }
-    } else if (event === "C") {
-        showList(vocabularyList);
-        let changeItem = prompt("Enter the name of what you want to Change?");
-        let index = vocabularyList.findIndex((a, i) => a.word === changeItem || i + 1 === +changeItem);
-        if (index !== -1) {
-            let name = prompt("What do you wanna Name it?");
-            let definition = prompt("What is the definition?");
-            vocabularyList[index] = { word: name, definition: definition };
-        }
-
-        else {
-            console.log("Word not found in the vocabulary list.");
-        }
+    let index = vocabularyList.findIndex((a, i) => a.word === deleteItem || i + 1 === +deleteItem);
+    if (index !== -1) {
+      vocabularyList.splice(index, 1);
     } else {
-        console.log("Invalid choice. Please try again.");
+      console.log('Word is not found in the vocabulary list.');
     }
+  } else if (event === 'C') {
+    showList(vocabularyList);
+    let changeItem = prompt('Enter the name of what you want to Change?');
+    let index = vocabularyList.findIndex((a, i) => a.word === changeItem || i + 1 === +changeItem);
+    if (index !== -1) {
+      let name = prompt('What do you wanna Name it?');
+      let definition = prompt('What is the definition?');
+      vocabularyList[index] = { word: name, definition: definition };
+    } else {
+      console.log('Word not found in the vocabulary list.');
+    }
+  } else {
+    console.log('Invalid choice. Please try again.');
+  }
 }
