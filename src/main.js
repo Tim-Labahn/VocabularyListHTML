dae6176f843d9a6703a88176e02d71f204eec4e5
tim-labahn <tim.labahn@freaknet.de>
2023-08-09 13:00:21 +0200

    delte functions works TODO: edit function

--- src/main.js
+++ src/main.js
@@ -5,22 +5,46 @@ function addToList() {
   const nameInput = document.getElementById('newVocName');
   /** @ts-expect-error @type HTMLInputElement*/
   const definitionInput = document.getElementById('newVocDef');
-
   vocabularyList.push({ name: nameInput.value, definition: definitionInput.value });
-  // showList();
+  showList();
 }
-
 function showList() {
-  const list = document.querySelector('.VocableList');
+  const list = document.querySelector('.VocabularyList');
   if (list) {
-    const tr = document.createElement('tr');
-    if (tr) {
+    list.innerHTML = '';
+    for (let i = 0; i < vocabularyList.length; i++) {
+      //--------------------------------------
+      const tr = document.createElement('tr');
+      const deftd = document.createElement('td');
+      const deltd = document.createElement('td');
+      const edittd = document.createElement('td');
+      const nametd = document.createElement('td');
+      const delButton = document.createElement('button');
+      const editButton = document.createElement('button');
+      //--------------------------------------
+      delButton.onclick = () => deleteVocabulary(i);
+      editButton.onclick = () => editVocabulary(i);
+      delButton.innerText = 'Delete';
+      editButton.innerText = 'Edit';
+      nametd.innerHTML = vocabularyList[i].name;
+      deftd.innerHTML = vocabularyList[i].definition;
       list.appendChild(tr);
-      for (let i = 0; i < vocabularyList.length; i++) {
-        const td = document.createElement('td');
-        td.innerHTML = vocabularyList[1].name + vocabularyList[1].definition;
-        tr.appendChild(td);
-      }
+      tr.appendChild(nametd);
+      tr.appendChild(deftd);
+      tr.appendChild(deltd);
+      tr.appendChild(edittd);
+      deltd.appendChild(delButton);
+      edittd.appendChild(editButton);
     }
   }
+}
+function deleteVocabulary(position) {
+  // let delPosition = 0;
+  vocabularyList.splice(position, 1);
+  showList();
+}
+function editVocabulary(position) {
+  // let delPosition = 0;
+  vocabularyList.splice(position, 1);
+  showList();
 }
